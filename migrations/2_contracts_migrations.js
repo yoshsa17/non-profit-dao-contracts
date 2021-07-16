f09bbecf0e9dc0f561940fc5fa1418b35d6c92d3
yshrsail <saiorsin44den@gmail.com>
2021-07-17 02:16:47 +0900

    refactor code

--- migrations/2_contracts_migrations.js
+++ migrations/2_contracts_migrations.js
@@ -1,11 +1,15 @@
 const NToken = artifacts.require("NToken");
-const NTreasury = artifacts.require("NTreasury")
+const NTreasury = artifacts.require("NTreasury");
 const NVoting = artifacts.require("NVoting");
 const NLock = artifacts.require("NLock");
 
-module.exports = async (deployer) => {
+// 365 days == 365days * 24hour * 60min * 60sec (epoch time)
+const maxLockTime = 365 * 24 * 60 * 60;
+const minLockTime = 0;
+
+module.exports = async deployer => {
   await deployer.deploy(NTreasury);
   await deployer.deploy(NToken, 10000);
-  await deployer.deploy(NLock, NToken.address);
+  await deployer.deploy(NLock, NToken.address, maxLockTime, minLockTime);
   await deployer.deploy(NVoting, NLock.address);
 };
