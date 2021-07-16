d76ad11fd61c79deb7fc7eb7e25a8515cf8f1977
yshrsail <saiorsin44den@gmail.com>
2021-07-17 02:17:42 +0900

    format test code

--- test/n_lock.js
+++ test/n_lock.js
@@ -1,64 +1,80 @@
 const NLock = artifacts.require("NLock");
 const NToken = artifacts.require("NToken");
 
-contract("NLock", async (accounts) => {
+contract("NLock", async accounts => {
   let nLock;
   let nToken;
-  const SIX_MONTH = 60*60*24*365/2;
-  const now = Math.round((new Date()).getTime() / 1000);
+  const SIX_MONTH = (60 * 60 * 24 * 365) / 2;
+  const now = Math.round(new Date().getTime() / 1000);
   beforeEach(async () => {
     nLock = await NLock.deployed();
     nToken = await NToken.deployed();
-  })
+  });
 
   describe("constructor", () => {
     it("should initialize all state data", async () => {
       const name = await nLock.name();
       const symbol = await nLock.symbol();
       const decimal = await nLock.decimal();
-      const totalLockedToken = await nLock.totalSupply()
+      const totalLockedToken = await nLock.totalSupply();
 
-      assert.equal(name, 'locked Non-profit DAO Token');
-      assert.equal(symbol, 'loNPO');
+      assert.equal(name, "locked Non-profit DAO Token");
+      assert.equal(symbol, "loNPO");
       assert.equal(decimal, 18);
       assert.equal(totalLockedToken, 0);
     });
-  })
+  });
 
   describe("balanceOf", () => {
-    it("should return 0 NLock",  async () => {
+    it("should return 0 NLock", async () => {
       const balance = await nLock.balanceOf(accounts[0]);
       assert.equal(balance, 0);
-    })
-  })
+    });
+  });
 
   describe("createLock", () => {
-    const unlockTime =  now+SIX_MONTH;
+    const unlockTime = now + SIX_MONTH;
 
-    it("should approve NLock.address", async () =>{
-      await nToken.approve(NLock.address , 10);
+    it("should approve NLock.address", async () => {
+      await nToken.approve(NLock.address, 10);
       const allowance = await nToken.allowance(accounts[0], NLock.address);
       assert.equal(allowance, 10);
     });
 
     it("should transfer from msg.sender to NLock.address", async () => {
       await nLock.createLock(10, unlockTime);
-      const nTokenBalanceOfAccount1 =await nToken.balanceOf(accounts[0]);
+      const nTokenBalanceOfAccount1 = await nToken.balanceOf(accounts[0]);
       const nTokenBalance = await nToken.balanceOf(NLock.address);
       assert.equal(nTokenBalanceOfAccount1, 9990);
       assert.equal(nTokenBalance, 10);
-    })
+    });
 
     it("should return voting power of accounts[0]", async () => {
       const balance = await nLock.balanceOf(accounts[0]);
       // 10 NPO * 6 month / 12 month = 5 loNPO
-      assert.equal(balance , 5)
-    })
-  })
+      assert.equal(balance, 5);
+    });
+  });
 
   describe("withdraw", () => {
-    // it("should withdraw the NToken from NLock ", async () => {
-
-    // });
+    it("should fail to withdraw with lock time", async () => {
+      let errMsg;
+      try {
+        await nLock.withdraw({ from: accounts[4] });
+      } catch (e) {
+        errMsg = e.reason;
+      }
+      assert.equal(errMsg, "NLock::This account has not locked NToken yet");
+    });
+    it("should fail to  withdraw", async () => {
+      let errMsg;
+      try {
+        await nLock.withdraw({ from: accounts[0] });
+      } catch (e) {
+        errMsg = e.reason;
+      }
+      assert.equal(errMsg, "NLock::The lock time didn't expired");
+    });
+    // it("should withdraw the NToken from NLock ", async () => {});
   });
 });
--- test/n_voting.js
+++ test/n_voting.js
@@ -3,10 +3,10 @@ const NLock = artifacts.require("NLock");
 const NToken = artifacts.require("NToken");
 
 const BN = web3.utils.BN;
-const SIX_MONTH = 60 * 60 * 24 * 365 / 2;
-const now = Math.round((new Date()).getTime() / 1000);
+const SIX_MONTH = (60 * 60 * 24 * 365) / 2;
+const now = Math.round(new Date().getTime() / 1000);
 
-contract("NVoting", async (accounts) => {
+contract("NVoting", async accounts => {
   let nVoting;
   let nLock;
   let nToken;
@@ -15,7 +15,7 @@ contract("NVoting", async (accounts) => {
     nVoting = await NVoting.deployed();
     nLock = await NLock.deployed();
     nToken = await NToken.deployed();
-  })
+  });
 
   describe("constructor", () => {
     it("should set correct value ", async () => {
@@ -29,18 +29,21 @@ contract("NVoting", async (accounts) => {
   });
 
   describe("createProposal and getProposal", () => {
-
-
     it("should create the first proposal", async () => {
       await nToken.approve(NLock.address, 10);
       await nLock.createLock(10, unlockTime);
 
       const ProposalUri = "foo";
       const votingOption = 1;
-      const startBlock = await web3.eth.getBlockNumber() + 1;
+      const startBlock = (await web3.eth.getBlockNumber()) + 1;
       const endBlock = startBlock + 10;
 
-      const id = await nVoting.createProposal(ProposalUri, votingOption, startBlock, endBlock);
+      const id = await nVoting.createProposal(
+        ProposalUri,
+        votingOption,
+        startBlock,
+        endBlock
+      );
       const count = await nVoting.proposalCount();
       const proposal = await nVoting.getProposal(1);
 
@@ -57,19 +60,25 @@ contract("NVoting", async (accounts) => {
       const p = await nVoting.getProposal(1);
       const blockNumber = await web3.eth.getBlockNumber();
       assert.equal(BN(p.startBlock).toString(), blockNumber);
-      assert.equal(status, 0)
-    })
+      assert.equal(status, 0);
+    });
 
     it("should create the second proposal", async () => {
       await nToken.transfer(accounts[1], 10);
-      await nToken.approve(NLock.address, 10 ,{from: accounts[1]});
-      await nLock.createLock(10, unlockTime,{from: accounts[1]});
+      await nToken.approve(NLock.address, 10, { from: accounts[1] });
+      await nLock.createLock(10, unlockTime, { from: accounts[1] });
       const ProposalUri = "hoge";
       const votingOption = 2;
-      const startBlock = await web3.eth.getBlockNumber() + 1;
+      const startBlock = (await web3.eth.getBlockNumber()) + 1;
       const endBlock = startBlock + 10;
 
-      const id = await nVoting.createProposal(ProposalUri, votingOption, startBlock, endBlock, {from: accounts[1]});
+      const id = await nVoting.createProposal(
+        ProposalUri,
+        votingOption,
+        startBlock,
+        endBlock,
+        { from: accounts[1] }
+      );
       const count = await nVoting.proposalCount();
       const proposal = await nVoting.getProposal(2);
 
@@ -80,7 +89,7 @@ contract("NVoting", async (accounts) => {
       assert.equal(proposal.startBlock, startBlock);
       assert.equal(proposal.endBlock, endBlock);
       assert.equal(proposal.canceled, false);
-    })
+    });
   });
 
   describe("CastVote and getProposalVotes", () => {
@@ -89,63 +98,59 @@ contract("NVoting", async (accounts) => {
       const p = await nVoting.getProposal(1);
       const status = await nVoting.getStatus(1);
       assert.isAbove(blockNumber, BN(p.startBlock).toNumber());
-      assert.equal(status, 1)
-    })
+      assert.equal(status, 1);
+    });
 
     it("should vote for the first proposal", async () => {
       await nToken.transfer(accounts[2], 10);
-      await nToken.approve(NLock.address, 10 ,{from: accounts[2]});
-      await nLock.createLock(10, unlockTime,{from: accounts[2]});
-      await nVoting.castVote(1, true ,{from: accounts[2]});
+      await nToken.approve(NLock.address, 10, { from: accounts[2] });
+      await nLock.createLock(10, unlockTime, { from: accounts[2] });
+      await nVoting.castVote(1, true, { from: accounts[2] });
 
       const totalVotes = await nVoting.getProposalVotes(1);
       assert.equal(totalVotes, 5);
-    })
+    });
 
     it("should fail to vote for the first proposal", async () => {
       let errMsg;
-      try{
-        await nVoting.castVote(1, true ,{from: accounts[2]});
-      }
-      catch(e){
+      try {
+        await nVoting.castVote(1, true, { from: accounts[2] });
+      } catch (e) {
         errMsg = e.reason;
       }
-      assert.equal(errMsg, "NVoting::You already voted for this proposal")
-    })
+      assert.equal(errMsg, "NVoting::You already voted for this proposal");
+    });
   });
 
   describe("Cancel", () => {
     it("should cancel the second proposal", async () => {
-      await nVoting.cancelProposal(2, {from: accounts[1]});
+      await nVoting.cancelProposal(2, { from: accounts[1] });
       const p = await nVoting.getProposal(2);
-      assert.equal(p.canceled,true);
-
-    })
+      assert.equal(p.canceled, true);
+    });
 
     it("should fail to cancel the second proposal", async () => {
       let errMsg;
-      try{
-        await nVoting.cancelProposal(1, {from: accounts[4]});
-      }
-      catch(e){
+      try {
+        await nVoting.cancelProposal(1, { from: accounts[4] });
+      } catch (e) {
         errMsg = e.reason;
       }
       const p = await nVoting.getProposal(1);
-      assert.equal(p.canceled,false);
+      assert.equal(p.canceled, false);
       assert.equal(errMsg, "NVoting::You are not a propose Owner");
-    })
+    });
   });
 
   describe("getStatus", () => {
     it("should get the first proposal status(Succeeded)", async () => {
-
       const blockNumber = await web3.eth.getBlockNumber();
       const p = await nVoting.getProposal(1);
       const status = await nVoting.getStatus(1);
       assert.isAbove(blockNumber, BN(p.startBlock).toNumber());
       assert.isAbove(blockNumber, BN(p.endBlock).toNumber());
       assert.equal(BN(status).toNumber(), 2);
-    })
+    });
     it("should get the first proposal status(Succeeded)", async () => {
       const status = await nVoting.getStatus(2);
       assert.equal(BN(status).toNumber(), 4);
