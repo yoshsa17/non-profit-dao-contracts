fa23d18d41ce88932d07b66dee90b2e04baefe18
yshrsail <saiorsin44den@gmail.com>
2021-07-30 00:17:27 +0900

    fix gas price and solc version

--- truffle-config.js
+++ truffle-config.js
@@ -43,12 +43,12 @@ module.exports = {
     // options below to some value.
     //
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "5777",   // Any network (default: none)
-      gas:4700000,
-      gasPrice: 100
-    },
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "5777", // Any network (default: none)
+      gas: 5500000,
+      gasPrice: 20000000000
+    }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -84,7 +84,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.6" // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
