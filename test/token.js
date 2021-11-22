54d52309d0c52940091b77dbeb7a7e5e86577a3d
Yoshiro Saito <saiorsin44den@gmail.com>
2021-11-22 20:55:52 +0900

    refactor: update test code

--- test/token.js
+++ test/token.js
@@ -1,56 +1,183 @@
+const {
+  BN,
+  constants,
+  expectEvent,
+  expectRevert,
+} = require("@openzeppelin/test-helpers");
+const { expect } = require("chai");
+const { toWei } = web3.utils;
 const NToken = artifacts.require("NToken");
 
 contract("NToken", async (accounts) => {
-  let instance;
+  const [CONTRACT_CREATOR, RECEIVER, , ...OTHERS] = accounts;
+  const INITIAL_TOKEN_AMOUNT = toWei("1000");
+  const ONE_TOKEN = toWei("1");
+  const TEN_TOKEN = toWei("10");
+  const ZERO_TOKEN = toWei("0");
+  const ZERO_ADDRESS = constants.ZERO_ADDRESS;
 
+  let token;
   beforeEach(async () => {
-    instance = await NToken.deployed();
+    token = await NToken.new({ from: CONTRACT_CREATOR });
   });
 
-  const INITIAL_TOKEN_AMOUNT = 10 ** 18 * 1000;
-  const ONE_TOKEN = 10 ** 18;
-  // const ZERO_ADDRESS = `0x0000000000000000000000000000000000000000`;
-
-  describe("constructor", () => {
-    it("should initialize all token state values", async () => {
-      const name = await instance.name();
-      const symbol = await instance.symbol();
-      const decimals = await instance.decimals();
-      const totalSupply = await instance.totalSupply();
-
-      assert.equal(name, "Non-profit DAO Token");
-      assert.equal(symbol, "NPO");
-      assert.equal(decimals.toNumber(), 18);
-      assert.equal(totalSupply, INITIAL_TOKEN_AMOUNT);
-    });
-
-    it("should put 1000 NToken in the msg.sender account", async () => {
-      const balance = await instance.balanceOf(accounts[0]);
-      assert.equal(balance, INITIAL_TOKEN_AMOUNT);
+  describe("constructor", async () => {
+    it("initializes token meta data", async () => {
+      expect(await token.name()).to.equal("Non-profit DAO Token");
+      expect(await token.symbol()).to.equal("NPO");
+      expect(await token.decimals()).to.be.bignumber.equal("18");
+      expect(await token.totalSupply()).to.be.bignumber.equal(
+        INITIAL_TOKEN_AMOUNT
+      );
     });
   });
 
-  describe("transfer", async () => {
-    it("should transfer 1 token from accounts[0] to accounts[1]", async () => {
-      await instance.transfer(accounts[1], BigInt(ONE_TOKEN));
-      const balanceOfAccount1 = await instance.balanceOf(accounts[1]);
-      assert.equal(balanceOfAccount1, ONE_TOKEN);
+  describe("balanceOf", async () => {
+    it("returns INITIAL_TOKEN_AMOUNT", async () => {
+      expect(await token.balanceOf(CONTRACT_CREATOR)).to.be.bignumber.equal(
+        INITIAL_TOKEN_AMOUNT
+      );
+    });
+
+    it("returns zero", async () => {
+      expect(await token.balanceOf(RECEIVER)).to.be.bignumber.equal("0");
+    });
+  });
+
+  describe("transfer", () => {
+    describe("CONTRACT_CREATOR transfers the requested amount", () => {
+      it("returns expected amounts", async () => {
+        await token.transfer(RECEIVER, new BN(ONE_TOKEN), {
+          from: CONTRACT_CREATOR,
+        });
+        expect(await token.balanceOf(CONTRACT_CREATOR)).to.be.bignumber.equal(
+          toWei("999")
+        );
+        expect(await token.balanceOf(RECEIVER)).to.be.bignumber.equal(
+          ONE_TOKEN
+        );
+      });
+    });
+
+    describe("events during transferring", () => {
+      it("emits a transfer event", async () => {
+        const { logs } = await token.transfer(RECEIVER, new BN(ONE_TOKEN), {
+          from: CONTRACT_CREATOR,
+        });
+
+        expectEvent.inLogs(logs, "Transfer", {
+          from: CONTRACT_CREATOR,
+          to: RECEIVER,
+          value: new BN(ONE_TOKEN),
+        });
+      });
+    });
+
+    describe("reverts during transferring", () => {
+      // InsufficientBalance()
+      it("reverts if the amount exceeds source's amount", async () => {
+        const balance = await token.balanceOf(CONTRACT_CREATOR);
+
+        expectRevert.unspecified(
+          token.transfer(RECEIVER, balance.add(new BN(ONE_TOKEN)), {
+            from: CONTRACT_CREATOR,
+          })
+        );
+      });
+
+      // ZeroAmount()
+      it("reverts if the amount is zero", async () => {
+        expectRevert.unspecified(
+          token.transfer(RECEIVER, new BN(ZERO_TOKEN), {
+            from: CONTRACT_CREATOR,
+          })
+        );
+      });
+
+      // ZeroAddress()
+      it("reverts if the target is Zero Address", async () => {
+        expectRevert.unspecified(
+          token.transfer(ZERO_ADDRESS, new BN(ONE_TOKEN), {
+            from: CONTRACT_CREATOR,
+          })
+        );
+      });
     });
   });
 
   describe("approve/transferFrom", () => {
-    it("should approve accounts[0] to transfer from account[1]", async () => {
-      await instance.approve(accounts[1], BigInt(ONE_TOKEN));
-      const allowance = await instance.allowance(accounts[0], accounts[1]);
-      assert.equal(allowance, ONE_TOKEN);
+    describe("OTHERS[0] transfers 10 NPO from CONTRACT_CREATOR to RECEIVER", () => {
+      it("approves 10 NPO to OTHERS[0]", async () => {
+        await token.approve(OTHERS[0], new BN(TEN_TOKEN), {
+          from: CONTRACT_CREATOR,
+        });
+        expect(
+          await token.allowance(CONTRACT_CREATOR, OTHERS[0])
+        ).to.be.bignumber.equal(TEN_TOKEN);
+      });
+
+      it("transfers 10 NPO from CONTRACT_CREATOR to RECEIVER", async () => {
+        await token.approve(OTHERS[0], new BN(TEN_TOKEN), {
+          from: CONTRACT_CREATOR,
+        });
+
+        await token.transferFrom(
+          CONTRACT_CREATOR,
+          RECEIVER,
+          new BN(TEN_TOKEN),
+          {
+            from: OTHERS[0],
+          }
+        );
+        expect(await token.balanceOf(CONTRACT_CREATOR)).to.be.bignumber.equal(
+          toWei("990")
+        );
+        expect(await token.balanceOf(RECEIVER)).to.be.bignumber.equal(
+          TEN_TOKEN
+        );
+      });
     });
 
-    it("should transfer from accounts[0] to account[2]", async () => {
-      await instance.transferFrom(accounts[0], accounts[2], BigInt(ONE_TOKEN), {
-        from: accounts[1],
+    describe("events during execution of approve/transferFrom", () => {
+      it("emits a approval event", async () => {
+        const { logs } = await token.approve(OTHERS[0], new BN(TEN_TOKEN), {
+          from: CONTRACT_CREATOR,
+        });
+
+        expectEvent.inLogs(logs, "Approval", {
+          owner: CONTRACT_CREATOR,
+          spender: OTHERS[0],
+          value: new BN(TEN_TOKEN),
+        });
       });
-      const balanceOfAccounts2 = await instance.balanceOf(accounts[2]);
-      assert.equal(balanceOfAccounts2, ONE_TOKEN);
+    });
+
+    describe("reverts during execution of approve/transferFrom", () => {
+      // ZeroAddress()
+      it("reverts if approval target is zero address", async () => {
+        expectRevert.unspecified(
+          token.approve(ZERO_ADDRESS, new BN(TEN_TOKEN), {
+            from: CONTRACT_CREATOR,
+          })
+        );
+      });
+
+      // InsufficientAllowance()
+      it("reverts if transfer amount > allowance", async () => {
+        await token.approve(OTHERS[0], new BN(TEN_TOKEN), {
+          from: CONTRACT_CREATOR,
+        });
+
+        expectRevert.unspecified(
+          token.transferFrom(
+            RECEIVER,
+            new BN(TEN_TOKEN).add(new BN(TEN_TOKEN)),
+            {
+              from: CONTRACT_CREATOR,
+            }
+          )
+        );
+      });
     });
   });
 });
